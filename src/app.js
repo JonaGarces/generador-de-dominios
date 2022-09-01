1bfba020459bdc61b4d890d403b653d47c44b793
JonaGarces <jonat.garces@gmail.com>
2022-09-01 21:42:44 +0000

    con-dominios

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
-alert("hola que hace");
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
 let pronoun = ["The", "Our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let dom = [".cl", ".com", ".net"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let o = 0; o < adj.length; o++) {
     for (let p = 0; p < noun.length; p++) {
-      console.log(pronoun[i] + adj[o] + noun[p]);
+      for (let a = 0; a < dom.length; a++) {
+        console.log(pronoun[i] + adj[o] + noun[p] + dom[a]);
+      }
     }
   }
 }
-console.log("hola");
